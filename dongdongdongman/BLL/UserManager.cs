34daf0296476fe526735c695093a8c3d261d10e2
永辉 施 <2786250969@qq.com>
2019-04-22 09:56:23 +0800

    sadadasd

---
+++ dongdongdongman/BLL/UserManager.cs
@@ -0,0 +1,19 @@
+﻿using DALFactory;
+using IDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+namespace BLL
+{
+    public class UserManager
+    {
+        IUser iuser = DataAccess.CreateUser();
+        public User FindUser(string name,string password)
+        {
+            return iuser.FindUser(name, password);
+        }
+    }
+}
